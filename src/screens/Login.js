c33dc3998736a29319dbac2b512cef054f975279
gabriel1325t <gabriel.carrasco1325@gmail.com>
2025-03-24 13:36:32 -0300

    24-03

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import { View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, Button } from "react-native";
 import api from '../axios/axios'
+import {Ionicons} from '@expo/vector-icons';
 
 export default function Login({ navigation }){
     const [user, setUser] = useState ({ 
         email: "",
         password: "",
+        showPassword:false
     });
 
     async function handleLogin(){
@@ -30,18 +32,24 @@ export default function Login({ navigation }){
             setUser({...user, email: value});
         }}
         />
+        <View style={styles.passwordContainer}>
         <TextInput
-        style={styles.input}
+        style={styles.passwordInput}
         placeholder="Senha"
+        secureTextEntry={user.showPassword}
         value={user.password}
         onChangeText={(value)=> {
             setUser({...user, password: value});
         }}
         />
+        <TouchableOpacity onPress={()=> setUser({...user,showPassword: !user.showPassword})}>
+            <Ionicons name={user.showPassword? "eye-off":"eye"} size={24} color= "purple"/>
+        </TouchableOpacity>
+        </View>
         <TouchableOpacity onPress={handleLogin} style={styles.button}>
             <Text>Entrar</Text>
         </TouchableOpacity>
-        <Button title="Cadastro" onPress={()=> navigation.navigate("Cadastro")}/>
+        <Button title="Cadastro" onPress={()=> navigation.navigate("Cadastro")} color="purple"/>
         </View>
     );
 }
@@ -66,6 +74,17 @@ export default function Login({ navigation }){
         backgroundColor: 'pink',
         padding:10,
         borderRadius:5
+    },
+    passwordContainer:{
+        flexDirection:"row",
+        alignItems:"center",
+        width:"100%",
+        borderBottomWidth:1,
+        paddingRight: 10,
+    },
+    passwordInput:{
+        flex:1,
+        height:40,
     }
 });
 
